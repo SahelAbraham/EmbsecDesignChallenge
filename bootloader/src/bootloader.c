7418c3e6f3f46b3419a0550a1ec15c8056546c4c
jjustinjc <caijjustin@gmail.com>
2023-07-26 16:12:43 +0000

    unfinished AES

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -14,7 +14,8 @@
 
 // Library Imports
 #include <string.h>
-
+#include "beaverssl.h"
+#include <stdio.h>
 // Application Imports
 #include "uart.h"
 
@@ -37,6 +38,9 @@ long program_flash(uint32_t, unsigned char *, unsigned int);
 #define ERROR ((unsigned char)0x01)
 #define UPDATE ((unsigned char)'U')
 #define BOOT ((unsigned char)'B')
+#define START_FRAME ((unsigned char)0x00)
+#define DATA_FRAME ((unsigned char)0x01)
+#define END_FRAME ((unsigned char)0x02)
 
 // Firmware v2 is embedded in bootloader
 // Read up on these symbols in the objcopy man page (if you want)!
@@ -51,6 +55,12 @@ void uart_write_hex_bytes(uint8_t uart, uint8_t * start, uint32_t len);
 
 // Firmware Buffer
 unsigned char data[FLASH_PAGESIZE];
+
+// Keys 
+// char AES_KEY[32]; //= ; 
+// char RSA_KEY[32]; //= ;
+
+FILE *fptr;
 
 int main(void){
 
@@ -80,6 +90,7 @@ int main(void){
         if (instruction == UPDATE){
             uart_write_str(UART1, "U");
             load_firmware();
+            //run decrypt functions 
             uart_write_str(UART2, "Loaded new firmware.\n");
             nl(UART2);
         }else if (instruction == BOOT){
@@ -88,7 +99,32 @@ int main(void){
         }
     }
 }
+void decrypt_aes(void){
+    //read key from file
+    fptr = fopen("main.axf", "rb");
+    char AES_KEY[32];
+    fgets(AES_KEY, 32, fptr);
+    fclose(fptr); 
 
+    //
+    char FW_SIZE[2];
+    uint16_t FW_SIZE_INT = FW_SIZE[0] + (FW_SIZE[1] << 8);
+    char FW_VER[2];
+    char MSG_SIZE[2];
+    uint16_t MSG_SIZE_INT = MSG_SIZE[0] + (MSG_SIZE[1] << 8);
+    char MSG_DATA[MSG_SIZE_INT];
+    char FW_DATA[FW_SIZE_INT];
+    char HASH[32];
+    char IV[16];
+
+
+    // aes_decrypt(AES_KEY, IV, MSG_DATA);
+
+
+}
+int bytes_to_int(){
+
+}
 /*
  * Load initial firmware into flash
  */
@@ -165,44 +201,66 @@ void load_firmware(void){
     int read = 0;
     uint32_t rcv = 0;
 
+
     uint32_t data_index = 0;
     uint32_t page_addr = FW_BASE;
     uint32_t version = 0;
     uint32_t size = 0;
+    uint32_t start_msg = 0;
 
-    // Get version as 16 bytes 
+    // Get 2 byte message type
     rcv = uart_read(UART1, BLOCKING, &read);
-    version = (uint32_t)rcv;
+    start_msg = (uint32_t)rcv;
     rcv = uart_read(UART1, BLOCKING, &read);
-    version |= (uint32_t)rcv << 8;
+    start_msg |= (uint32_t)rcv << 8;
 
-    uart_write_str(UART2, "Received Firmware Version: ");
-    uart_write_hex(UART2, version);
-    nl(UART2);
-
-    // Get size as 16 bytes 
-    rcv = uart_read(UART1, BLOCKING, &read);
-    size = (uint32_t)rcv;
-    rcv = uart_read(UART1, BLOCKING, &read);
-    size |= (uint32_t)rcv << 8;
-
-    uart_write_str(UART2, "Received Firmware Size: ");
-    uart_write_hex(UART2, size);
-    nl(UART2);
-
-    // Compare to old version and abort if older (note special case for version 0).
-    uint16_t old_version = *fw_version_address;
-
-    if (version != 0 && version < old_version){
-        uart_write(UART1, ERROR); // Reject the metadata.
-        SysCtlReset();            // Reset device
-        return;
+    if(start_msg == 1){
+        rcv = uart_read(UART1, BLOCKING, &read);
+        start_msg = (uint32_t)rcv;
+        rcv = uart_read(UART1, BLOCKING, &read);
+        start_msg |= (uint32_t)rcv << 8;
     }
 
-    if (version == 0){
-        // If debug firmware, don't change version
-        version = old_version;
+    if(start_msg == 1){
+        rcv = uart_read(UART1, BLOCKING, &read);
+        start_msg = (uint32_t)rcv;
+        rcv = uart_read(UART1, BLOCKING, &read);
+        start_msg |= (uint32_t)rcv << 8;
     }
+    
+    // // Get version as 16 bytes 
+    // rcv = uart_read(UART1, BLOCKING, &read);
+    // version = (uint32_t)rcv;
+    // rcv = uart_read(UART1, BLOCKING, &read);
+    // version |= (uint32_t)rcv << 8;
+
+    // uart_write_str(UART2, "Received Firmware Version: ");
+    // uart_write_hex(UART2, version);
+    // nl(UART2);
+
+    // // Get size as 16 bytes 
+    // rcv = uart_read(UART1, BLOCKING, &read);
+    // size = (uint32_t)rcv;
+    // rcv = uart_read(UART1, BLOCKING, &read);
+    // size |= (uint32_t)rcv << 8;
+
+    // uart_write_str(UART2, "Received Firmware Size: ");
+    // uart_write_hex(UART2, size);
+    // nl(UART2);
+
+    // // Compare to old version and abort if older (note special case for version 0).
+    // uint16_t old_version = *fw_version_address;
+
+    // if (version != 0 && version < old_version){
+    //     uart_write(UART1, ERROR); // Reject the metadata.
+    //     SysCtlReset();            // Reset device
+    //     return;
+    // }
+
+    // if (version == 0){
+    //     // If debug firmware, don't change version
+    //     version = old_version;
+    // }
 
     // Write new firmware size and version to Flash
     // Create 32 bit word for flash programming, version is at lower address, size is at higher address

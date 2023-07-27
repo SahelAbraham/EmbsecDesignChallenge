def7b3767a03b8d0d95466d68ff22ec41e3b3603
Sahel Abraham <abrahamjsahel@gmail.com>
2023-07-27 18:54:48 +0000

    actually finished bl_build fr

--- tools/bl_build.py
+++ tools/bl_build.py
@@ -84,14 +84,14 @@ if __name__ == "__main__":
     open(path, 'w').close()
     file = open(path, 'w')
     file.write("#ifndef main.h\n#define main.h\n")
-    file.write('char cbckey[] = {') #writes cbc key to the header file with "C" syntax
+    file.write('const char cbckey[] = {') #writes cbc key to the header file with "C" syntax
     for x in cbckey:
         file.write('\'')
         file.write(x)
         file.write('\',')
     file.write('};')
     file.write('\n')
-    file.write('char gcmkey[] = {') #writes gcm key to the header file with "C" syntax
+    file.write('const char gcmkey[] = {') #writes gcm key to the header file with "C" syntax
     for x in gcmkey:
         file.write('\'')
         file.write(x)

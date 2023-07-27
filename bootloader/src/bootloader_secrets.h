00ef4fd6f28ec9388ba2b3f4bfa8add5ae93163c
KeyboardAbuser <kennyyu203007@gmail.com>
2023-07-27 19:05:51 +0000

    ASDASDASDADA

---
+++ bootloader/src/bootloader_secrets.h
@@ -0,0 +1,5 @@
+#ifndef main.h
+#define bootloader_secrets.h
+const char cbckey[] = {'n','q','Q',']','^','7','$','n','y','7','=','L','g','W','L','!','7','5','$','t','e','7','_','%','f','i','K','X','T',')','.','N',};
+const char gcmkey[] = {'i','"','J','v','y','O','E','j','r','v','P','W','b','z','9','n','a','o','o','c','9','z','f','Q','L',',','[',']','6','n','3',',',};
+#endif
